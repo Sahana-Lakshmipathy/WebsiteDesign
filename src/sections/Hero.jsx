d355774ac354c06e3d5df647d5fc90ea801c4938
Sahana-Lakshmipathy <sahana.lp100@gmail.com>
2024-07-13 20:37:08 +0530

    till navbar

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,27 @@
+
+
+const Hero = () => {
+  return (
+    <section
+    id="home"
+    className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
+
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl: padding-x pt-28">
+        <p>Our Summer Collections</p>
+
+        <h1>
+          <span>
+            The New Arrival
+          </span>
+          <span>Nike</span> 
+          Shoes
+        </h1>
+
+        <p>Discover stylish Nike arraivals, quality comfort and innovation for your active life.</p>
+      </div>
+
+    </section>
+  )
+}
+
+export default Hero
